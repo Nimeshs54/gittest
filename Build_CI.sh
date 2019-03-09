aec9c23761f5a248216815588c21165a1db89f6c
manu <manu@localhost.localdomain>
2019-03-09 16:11:37 +0530

    Testing CI

--- Build_CI.sh
+++ Build_CI.sh
@@ -1,13 +1,13 @@
 wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
-tar xf gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz && mv gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf aarch64-linux-gnu
-export CROSS_COMPILE="/usr/bin/ccache ./aarch64-linux-gnu/bin/aarch64-linux-gnu-" ;
+tar xf gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz && mv gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf arm-linux-gnueabihf
+export CROSS_COMPILE="/usr/bin/ccache ./arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" ;
 export ARCH=arm;
 export SUBARCH=arm;
 export KBUILD_BUILD_USER="K-hawk";
 export KBUILD_BUILD_HOST="Peace_boys";
-STRIP="aarch64-linux-gnu/bin/aarch64-linux-gnu-strip";
+STRIP="arm-linux-gnueabihf/bin/arm-linux-gnueabihf-strip";
 export CCOMPILE=$CROSS_COMPILE;
-export CROSS_COMPILE="/usr/bin/ccache aarch64-linux-gnu-" ;
+export CROSS_COMPILE="/usr/bin/ccache arm-linux-gnueabihf-" ;
 export PATH=$PATH:./aarch64-linux-gnu/bin/ ;
 make haruhi_defconfig;
 make -j$(nproc --all);
