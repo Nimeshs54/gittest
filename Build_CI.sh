cf414839ae26f89df9ad5eb632f3b9095a191855
manu <manu@localhost.localdomain>
2019-03-09 16:44:00 +0530

    $Path FIX

--- Build_CI.sh
+++ Build_CI.sh
@@ -1,7 +1,7 @@
 wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
 tar xf gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz && mv gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf arm-linux-gnueabihf
 ls -l
-export CROSS_COMPILE="/usr/bin/ccache arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" ;
+export CROSS_COMPILE="/usr/bin/ccache ~/arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" ;
 export ARCH=arm;
 export SUBARCH=arm;
 export KBUILD_BUILD_USER="K-hawk";
@@ -10,6 +10,6 @@ STRIP="arm-linux-gnueabihf/bin/arm-linux-gnueabihf-strip";
 export CCOMPILE=$CROSS_COMPILE;
 export CROSS_COMPILE="/usr/bin/ccache arm-linux-gnueabihf-" ;
 pwd
-export PATH=$PATH:arm-linux-gnueabihf/bin ;
+export PATH=$PATH:~/arm-linux-gnueabihf/bin ;
 make alldefconfig;
 make -j$(nproc --all);
