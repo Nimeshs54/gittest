9180cf508d1fa0ce17ec2d35b92d561fe2621883
manu <manu@localhost.localdomain>
2019-03-09 17:32:10 +0530

    c

--- Build_CI.sh
+++ Build_CI.sh
@@ -1,6 +1,6 @@
 wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
 tar xf gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz && mv gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf arm-linux-gnueabihf
-export CROSS_COMPILE="/usr/bin/ccache arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" ;
+export CROSS_COMPILE="/usr/bin/ccache $HOME/travis/manu490/GT-I9082_Stock_Kernel/arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" ;
 export ARCH=arm;
 export SUBARCH=arm;
 export KBUILD_BUILD_USER="K-hawk";
