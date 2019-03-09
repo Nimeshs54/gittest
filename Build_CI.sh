d031730f7ef94184341f30f29408dcc07bb4a4d8
manu <manu@localhost.localdomain>
2019-03-09 15:26:51 +0530

    fixing Travis

--- Build_CI.sh
+++ Build_CI.sh
@@ -1,6 +1,6 @@
 wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
 tar xf gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
-export CROSS_COMPILE="/usr/bin/ccache ./gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" ;
+export CROSS_COMPILE="/usr/bin/ccache gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" ;
 export ARCH=arm;
 export SUBARCH=armhf;
 export KBUILD_BUILD_USER="K-hawk";
