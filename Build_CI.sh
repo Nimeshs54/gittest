a4500dddec128ff20ee997988535e91440b0cbae
manu <manu@localhost.localdomain>
2019-03-09 05:45:22 +0530

    Don't Edit #CI

---
+++ Build_CI.sh
@@ -0,0 +1,16 @@
+mkdir ~/aarch32-toolchain
+cd ~/aarch32-toolchain
+wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
+tar xf gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
+export PATH=$PATH:$PWD/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin
+export CROSS_COMPILE="/usr/bin/ccache ./gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" ;
+export ARCH=arm;
+export SUBARCH=armhf;
+export KBUILD_BUILD_USER="K-hawk";
+export KBUILD_BUILD_HOST="Peace_boys";
+STRIP="gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-strip";
+export CCOMPILE=$CROSS_COMPILE;
+export CROSS_COMPILE="/usr/bin/ccache arm-linux-gnueabihf-" ;
+export PATH=$PATH:./gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin/ ;
+make haruhi_defconfig;
+make -j$(nproc --all);
