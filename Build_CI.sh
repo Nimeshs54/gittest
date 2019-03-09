aa4219d8035730ad85a6459be5058fe4b2e551aa
manu <manu@localhost.localdomain>
2019-03-09 07:02:36 +0530

    ..

--- Build_CI.sh
+++ Build_CI.sh
@@ -1,5 +1,3 @@
-mkdir ~/aarch32-toolchain
-cd ~/aarch32-toolchain
 wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
 tar xf gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
 export PATH=$PATH:$PWD/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin
@@ -12,8 +10,6 @@ STRIP="gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin/arm-linux-gnueabihf
 export CCOMPILE=$CROSS_COMPILE;
 export CROSS_COMPILE="/usr/bin/ccache arm-linux-gnueabihf-" ;
 export PATH=$PATH:./gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin/ ;
-cd ..
-ls
 make vexpress_defconfig;
 make -j$(nproc --all);
 # Don't edit #travisCI
