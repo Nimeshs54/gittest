2b80c9829ea58a606795bb37a5dcf24c929c9f1b
manu <manu@localhost.localdomain>
2019-03-09 15:57:50 +0530

    experimental

--- Build_CI.sh
+++ Build_CI.sh
@@ -1,16 +1,12 @@
-wget https://releases.linaro.org/components/toolchain/binaries/latest-7/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
-tar xf gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf.tar.xz
-ls -l
-pwd
-export CROSS_COMPILE="/usr/bin/ccache gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-" ;
-export ARCH=arm;
-export SUBARCH=armhf;
-export KBUILD_BUILD_USER="K-hawk";
-export KBUILD_BUILD_HOST="Peace_boys";
-STRIP="gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-strip";
+git clone https://bitbucket.org/anupritaisno1/aarch64-linux-gnu -b linaro --single-branch --depth=1 -4 aarch64-linux-gnu -j$(nproc --all);
+export CROSS_COMPILE="/usr/bin/ccache ./aarch64-linux-gnu/bin/aarch64-linux-gnu-" ;
+export ARCH=arm64;
+export SUBARCH=arm64;
+export KBUILD_BUILD_USER="Suzumiya";
+export KBUILD_BUILD_HOST="The_literary_club";
+STRIP="aarch64-linux-gnu/bin/aarch64-linux-gnu-strip";
 export CCOMPILE=$CROSS_COMPILE;
-export CROSS_COMPILE="/usr/bin/ccache arm-linux-gnueabihf-" ;
-export PATH=$PATH:/gcc-linaro-7.4.1-2019.02-i686_arm-linux-gnueabihf/bin ;
-make vexpress_defconfig;
+export CROSS_COMPILE="/usr/bin/ccache aarch64-linux-gnu-" ;
+export PATH=$PATH:./aarch64-linux-gnu/bin/ ;
+make haruhi_defconfig;
 make -j$(nproc --all);
-# Don't edit #travisCI
